de651a5c87107ede880f55a79d2d45f908524432
Jorge Martínez <jorgemartinez.jam@gmail.com>
2025-11-08 18:08:25 -0300

    fix: register Anomaly and Wallet entities in WebhooksModule to fix 500 errors in webhook endpoints

--- backend/src/webhooks/webhooks.module.ts
+++ backend/src/webhooks/webhooks.module.ts
@@ -1,10 +1,16 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { WebhooksService } from "./webhooks.service";
 import { WebhooksController } from "./webhooks.controller";
 import { DatabaseModule } from "../database/database.module";
+import { Anomaly } from "../entities/anomaly.entity";
+import { Wallet } from "../entities/wallet.entity";
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    TypeOrmModule.forFeature([Anomaly, Wallet]),
+  ],
   providers: [WebhooksService],
   controllers: [WebhooksController],
 })
